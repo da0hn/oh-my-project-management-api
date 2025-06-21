c7d855ead2fb312abac6be3415bc64ad8dee28f2
Gabriel Honda <gabrielhondacba@gmail.com>
2025-06-21 16:07:02 -0400

    feat: adiciona estrutura inicial do projeto
    - Inclui configuração do Maven e dependências no `pom.xml`.
    - Adiciona `Application` e `UserController` na estrutura do projeto.
    - Configura arquivos `.gitignore` e `.gitattributes` para o repositório.
    - Introduz suporte ao Docker Compose com serviço para banco PostgreSQL.

---
+++ src/main/java/dev/ghonda/project/management/Application.java
@@ -0,0 +1,13 @@
+package dev.ghonda.project.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ghonda/project/management/Application.java
@@ -0,0 +1,13 @@
+package dev.ghonda.project.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
